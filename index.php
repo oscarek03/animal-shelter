19ddc80f6f1a811c934e8236d43fce19c35869c8
Oscar <ob131409@stud.ur.edu.pl>
2024-10-21 22:00:51 +0200

    Dodany prosty navbar z podstawowymi przyciskami (na razie bez zadnych funkcjonalnosci). Dodana biblioteka bootstrap dodajaca style oraz skrypty. Poprawione logo.

--- index.php
+++ index.php
@@ -4,9 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Schronisko dla zwierząt w Rzeszowie</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css\bootstrap.min.css">
+    <link rel="stylesheet" href="css\style.css">
+    <script defer src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/scripts.js"></script>
+
 </head>
+<!-- Fajne kolory
+03045e
+0077b6
+00b4d8
+90e0ef
+caf0f8 (kolor navbaru)
+-->
 <body>
-    
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <a class="navbar-brand" href="#"><img src="img/logo.png" width="130px" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <input type="checkbox" id="menu_checkbox">
+                    <label for="menu_checkbox">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </label>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ms-auto"> 
+                        <a class="nav-item nav-link active" href="#">Aktualności</a>
+                        <a class="nav-item nav-link active" href="#">Zwierzęta</a>
+                        <a class="nav-item nav-link active" href="#">Adopcje</a>
+                        <a class="nav-item nav-link active" href="#">Darowizny</a>
+                        <a class="nav-item nav-link active" href="#">O nas</a>
+                        <a class="nav-item nav-link active" href="#">Kontakt</a>
+                    </div>
+                </div>
+            </nav>  
+        </div>
+    </div>
 </body>
 </html>
